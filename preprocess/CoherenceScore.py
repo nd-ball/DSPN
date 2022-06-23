bd72600f7e0d8347f66fc34489e22ff411eb00a6
Chris <103910994+WenchangLi1997@users.noreply.github.com>
2022-06-23 04:25:23 -0400

    Add files via upload

---
+++ preprocess/CoherenceScore.py
@@ -0,0 +1,63 @@
+from itertools import combinations
+from math import log
+import tqdm
+
+class coherence_score():
+    def __init__(self, filepath):
+        # 生成包含所有需要的频率的字典
+        self.dic = {}
+
+        for line in tqdm.tqdm(open(filepath, encoding="utf-8")):
+            words = list(set(line.strip().split()))
+            # wl
+            for w in words:
+                if w in self.dic:
+                    self.dic[w] += 1
+                else:
+                    self.dic[w] = 1
+            
+            # wn
+            com_list = list(combinations(words, 2))
+            for c in com_list:
+                if c[0] < c[1]:
+                    ws = c[0]
+                    wb = c[1]
+                else:
+                    ws = c[1]
+                    wb = c[0]
+
+                key = ws + "_" + wb
+                if key in self.dic:
+                    self.dic[key] += 1
+                else:
+                    self.dic[key] = 1
+
+
+    def get_D1_D2(self, wl, wn):
+        d1 = d2 = 0
+        if wl in self.dic:
+            d1 = self.dic[wl]
+        
+        if wl < wn:
+            ws = wl
+            wb = wn
+        else:
+            ws = wn
+            wb = wl
+        if ws + "_" + wb in self.dic:
+            d2 = self.dic[ws+"_"+wb]
+        
+        return d1, d2  
+      
+
+    # topn <= 50
+    def get_co_score(self, words, topn):
+        words = words[:topn]
+        N = len(words)
+        score = 0
+        for n in range(2, N+1):
+            for l in range(1, n):
+                d1, d2 = self.get_D1_D2(words[l-1], words[n-1])
+                score += log((d2+1)/d1)
+        return score
+
---
+++ preprocess/Word2vec.py
@@ -0,0 +1,64 @@
+from sklearn.cluster._kmeans import MiniBatchKMeans
+from sklearn.cluster import KMeans
+import numpy as np
+import gensim
+import codecs
+import tqdm
+import os
+
+class word2vec:
+    #def __init__(self, corpus_path):
+    #    self.corpus_path = corpus_path
+    #    self.n_vocab = 0
+
+    #def __iter__(self):
+    #    with codecs.open(self.corpus_path, 'r', 'utf-8') as f:
+    #        for line in tqdm.tqdm(f, desc='training'):
+    #            yield line.split()
+
+    def __init__(self, sentences):
+        self.sentences = sentences
+        self.n_vocab = 0
+
+    def __iter__(self):
+        for s in tqdm.tqdm(self.sentences, desc='training'):
+            yield s.split()
+    
+
+    def add_word(self, *words):
+        for word in words:
+            if not word in self.w2i:
+                self.w2i[word] = self.n_vocab
+                self.i2w[self.w2i[word]] = word
+                self.n_vocab += 1
+
+    def embed(self, model_path, d_embed, max_n_vocab=None, window=5, min_count=10, workers=16):
+        self.d_embed = d_embed
+        if os.path.isfile(model_path):
+            model = gensim.models.Word2Vec.load(model_path)
+        else:
+            model = gensim.models.Word2Vec(self, size=d_embed, max_final_vocab=max_n_vocab, 
+                                           window=window, min_count=min_count, workers=workers)
+            model.save(model_path)
+            model = gensim.models.Word2Vec.load(model_path)
+        self.i2w, self.w2i = {}, {}
+        self.E = []
+        for word in sorted(model.wv.vocab):
+            self.add_word(word)
+            self.E.append(list(model.wv[word])) # E是每个词的向量，维度是(vocab_size+2, emb_size)，+2是<unk>和<pad>
+        else: # 如果上面这个for循环没有发生break，那么就会执行else的语句
+            self.add_word('<unk>')
+            self.E.append([0] * d_embed)
+            self.add_word('<pad>')
+            self.E.append([0] * d_embed)
+        self.E = np.asarray(self.E).astype(np.float32)
+        return self
+
+    def aspect(self, n_aspects):
+        self.n_aspects = n_aspects
+        # km = KMeans(n_clusters=n_aspects, random_state=0)
+        km = MiniBatchKMeans(n_clusters=n_aspects, random_state=0)
+        km.fit(self.E)
+        self.T = km.cluster_centers_.astype(np.float32)
+        self.T /= np.linalg.norm(self.T, axis=-1, keepdims=True)
+        return self
