b3db63557e579fd4436df9218847adbca48e10c3
WenchangLi <103910994+WenchangLi1997@users.noreply.github.com>
2025-04-02 20:21:49 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,18 @@
+import random
+import numpy as np
+import torch
+import os
+
+
+def set_seed(seed_value):
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    os.environ['PYTHONHASHSEED'] = str(seed_value)
+    
+    if torch.cuda.is_available(): 
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
